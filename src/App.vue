97c7865be16d746fe2763b7fb4f87b9d08665b46
f26401004 <f26401004@gmail.com>
2020-12-15 22:05:34 +0800

    Add Pug and Sass preprocessors

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="pug">
+div
+  h1 Chun-Hao Huang
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Lato', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
