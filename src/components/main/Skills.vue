28ea9f5d10dd7d527a5afd551c9cca0001b133d4
f26401004 <f26401004@gmail.com>
2020-12-23 21:51:22 +0800

    Fill the skill data and add collasible items to describe personal skills with certain keywords

--- src/components/main/Skills.vue
+++ src/components/main/Skills.vue
@@ -19,9 +19,34 @@ v-card.rounded-lg.mt-10.pb-5
     v-container
       h4.text-h6.text-center.font-weight-black.white--text {{ currentSkill }}
   v-container
-    v-tabs-items.pl-5.pr-5.mt-3(v-model="currentSkillIndex")
-      v-tab-item(v-for="(type, index) of Object.keys(skillData)"
-        :key="`skill-tab-${type}-${index}`") {{ type }}
+    v-tabs-items.pa-1.mt-3(v-model="currentSkillIndex")
+      v-tab-item(v-for="(type, tidx) of Object.keys(skillData)"
+        :key="`skill-tab-${type}-${tidx}`")
+        v-expansion-panels
+          v-expansion-panel(v-for="(skill, sidx) of skillData[currentSkill]"
+            :key="`${currentSkill}-${skill.name}-${sidx}`")
+            v-expansion-panel-header.pt-3.pb-3.pl-4.pr-4
+              v-row.pa-0.ma-0(justify="center"
+                align="center")
+                v-col.pa-0.ma-0(cols="2")
+                  v-icon(color="primary"
+                    dense) {{ skill.icon }}
+                v-col.pa-0.ma-0(cols="10")
+                  p.mb-1.font-weight-bold.primary--black {{ skill.name }}
+                  v-rating(readonly
+                    dense
+                    v-model="skill.level"
+                    length="10"
+                    size="14px"
+                    background-color="grey darken-1"
+                    color="secondary")
+            v-expansion-panel-content
+              v-row(justify="start")
+                v-chip.ma-1(v-for="(label, lidx) of skill.labels"
+                  :key="`${currentSkill}-${label}-${lidx}`"
+                  outlined
+                  small
+                  color="primary") {{ label }} 
 </template>
 
 <script>
@@ -32,12 +57,13 @@ export default {
   data: function () {
     return {
       skillData,
-      currentSkillIndex: 0
+      currentSkillIndex: 0,
     }
   },
   computed: {
     currentSkill: function () {
       const types = Object.keys(this.skillData)
+      console.log(this.skillData[types[this.currentSkillIndex]][0])
       return types[this.currentSkillIndex]
     }
   }
@@ -49,7 +75,7 @@ export default {
 .custom-skill-preview {
   width: 100%;
   height: 100%;
-  background-size: 65%;
+  background-size: contain;
   background-position: center;
   background-repeat: no-repeat;
 
